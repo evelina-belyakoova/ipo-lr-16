8b9c477e200e1048352a4b0d08653f4802b83c1a
Evelina <belakovaevelina84@gmail.com>
2026-03-03 17:23:27 +0300

    Лаба 16, сайт в разработке

---
+++ officesupple_shop/officesupple_shop/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from shop import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name = 'index'),
+    path('about/', views.about, name = 'about'),
+    path('shop-info/', views.shop_info, name = 'shop_info'),
+]
